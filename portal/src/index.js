214a97dc38e047409a38fcbe0d9423d4701e64a4
kboul <kos.voulgaridis@gmail.com>
2019-10-21 16:04:01 +0300

    Setup redux bolierplate & create dummy successReducer on portal

--- portal/src/index.js
+++ portal/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -7,6 +9,11 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
---
+++ portal/src/store/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { successReducer } from './successReducer';
+
+export const rootReducer = combineReducers({
+    success: successReducer
+});
+
+export default rootReducer;
