30d4de5d73a6d32a349b1198bceb44e8ca75525d
kboul <kos.voulgaridis@gmail.com>
2019-10-21 11:42:15 +0300

    Create Congrats component & display two divs depending on success prop value, include tests

---
+++ portal/src/components/Congrats.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Congrats = ({ success }) => {
+    return success ? (
+        <div data-test="component-congrats">
+            <span data-test="congrats-message">
+                Congratulations! You guessed the word!
+            </span>
+        </div>
+    ) : (
+        <div data-test="component-congrats"></div>
+    );
+};
+
+Congrats.propTypes = {
+    success: PropTypes.bool
+};
+
+export default Congrats;
