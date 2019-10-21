22c783dbc5c80dee7e31da84b54c79dcc3c5f368
kboul <kos.voulgaridis@gmail.com>
2019-10-21 11:07:04 +0300

    Create findByTestAttr function

---
+++ portal/test/testUtils.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
+ * @param {*} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test=${val}]`);
+};
