6ca720fe91e2bdad561957d040ba839bba88f2fa
kboul <kos.voulgaridis@gmail.com>
2019-10-21 12:54:27 +0300

    Create a div that is always visible and a span child which is visible only when guessedWords are empty and test them both on portal's GuessedWords

--- portal/src/components/GuessedWords.jsx
+++ portal/src/components/GuessedWords.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuessedWords = () => {
-    return <div></div>;
+const GuessedWords = ({ guessedWords }) => {
+    let contents;
+
+    !guessedWords.length
+        ? (contents = (
+              <span data-test="guess-instructions">
+                  Try to guess the secret word
+              </span>
+          ))
+        : null;
+
+    return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessedWords.propTypes = {
--- portal/src/components/__tests__/GuessedWords.test.js
+++ portal/src/components/__tests__/GuessedWords.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import GuessedWords from '../GuessedWords';
-import { checkProps } from '../../../test/testUtils';
+import { checkProps, findByTestAttr } from '../../../test/testUtils';
 
 const defaultProps = {
     guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
@@ -15,3 +15,21 @@ const setup = (props = {}) => {
 test('does not throw warning with expected props', () => {
     checkProps(GuessedWords, defaultProps);
 });
+
+describe('if there are no words guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] });
+    });
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component).toHaveLength(1);
+    });
+
+    test('renders instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text()).not.toHaveLength(0);
+    });
+});
+
+describe('if there are words guessed', () => {});
