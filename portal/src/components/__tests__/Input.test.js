c8da6dfb19362dfca8e5279342b11b5d7a0aaf48
kboul <kos.voulgaridis@gmail.com>
2019-10-22 15:40:07 +0300

    Place tests for word has been guessed to word has not been guessed & create the tests for word has been guessed on portals Inout.test

--- portal/src/components/__tests__/Input.test.js
+++ portal/src/components/__tests__/Input.test.js
@@ -16,14 +16,6 @@ setup();
 
 describe('render', () => {
     describe('word has not been guessed', () => {
-        test('renders component without error', () => {});
-
-        test('renders input box', () => {});
-
-        test('renders submit button', () => {});
-    });
-
-    describe('word has been guessed', () => {
         let wrapper;
         beforeEach(() => {
             const initialState = { success: false };
@@ -34,14 +26,36 @@ describe('render', () => {
             expect(component).toHaveLength(1);
         });
 
-        test('does not render input box', () => {
+        test('renders input box', () => {
             const component = findByTestAttr(wrapper, 'input-box');
             expect(component).toHaveLength(1);
         });
 
-        test('does not render submit button', () => {
+        test('renders submit button', () => {
             const component = findByTestAttr(wrapper, 'submit-button');
             expect(component).toHaveLength(1);
+        });
+    });
+
+    describe('word has been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: true };
+            wrapper = setup(initialState);
+        });
+        test('renders component without error', () => {
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component).toHaveLength(1);
+        });
+
+        test('does not render input box', () => {
+            const component = findByTestAttr(wrapper, 'input-box');
+            expect(component).toHaveLength(0);
+        });
+
+        test('does not render submit button', () => {
+            const component = findByTestAttr(wrapper, 'submit-button');
+            expect(component).toHaveLength(0);
         });
     });
 });
