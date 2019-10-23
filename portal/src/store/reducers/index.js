00f417af0f5fcb99e944b0b635953e1689b305e9
kboul <kos.voulgaridis@gmail.com>
2019-10-23 11:44:42 +0300

    Create guessedWordsReducer & import it on rootReducer on portal

--- portal/src/store/reducers/index.js
+++ portal/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { successReducer } from './successReducer';
+import { guessedWordsReducer } from './guessedWordsReducer';
 
 export const rootReducer = combineReducers({
-    success: successReducer
+    success: successReducer,
+    guessedWords: guessedWordsReducer
 });
 
 export default rootReducer;
