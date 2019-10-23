c2e06d8632471c02641a4c529c82e9c30b726668
kboul <kos.voulgaridis@gmail.com>
2019-10-23 14:36:47 +0300

    Create guessWord action creator & complete guessedWordsReducer (both handle GUESS_WORD action type) & write an integration test to test them both together

--- portal/src/store/actions/index.js
+++ portal/src/store/actions/index.js
@@ -1,5 +1,18 @@
 import { types } from './types';
+import { getLetterMatchCount } from '../../utils/getLetterMatchCount';
 
 export const guessWord = guessedWord => {
-    return (dispatch, getState) => {};
+    return (dispatch, getState) => {
+        const secretWord = getState().secretWord;
+        const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+        dispatch({
+            type: types.GUESS_WORD,
+            payload: { guessedWord, letterMatchCount }
+        });
+
+        if (guessedWord === secretWord) {
+            dispatch({ type: types.CORRECT_GUESS });
+        }
+    };
 };
--- portal/src/store/reducers/guessedWordsReducer.js
+++ portal/src/store/reducers/guessedWordsReducer.js
@@ -1,3 +1,10 @@
-export const guessedWordsReducer = (state, action) => {
-    return null;
+import { types } from '../actions/types';
+
+export const guessedWordsReducer = (state = [], action) => {
+    switch (action.type) {
+        case types.GUESS_WORD:
+            return [...state, action.payload];
+        default:
+            return state;
+    }
 };
