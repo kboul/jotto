8938e11034747e09bb2b6ae2d6a888b0f92425e4
kboul <kos.voulgaridis@gmail.com>
2019-10-24 14:35:33 +0300

    Disable lifecycle methods on setupTests

--- portal/src/setupTests.js
+++ portal/src/setupTests.js
@@ -1,4 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+configure({
+    adapter: new Adapter(),
+    // disable lifecycle methods during test
+    // they will run whenever WE call them
+    disableLifecycleMethods: true
+});
