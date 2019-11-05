629bcc1e5018e1e2b2a4788eb9a564c329ee3010
kboul <kos.voulgaridis@gmail.com>
2019-11-05 10:54:32 +0200

    Create a form with an input & a button & change its value using useState hook on Input, include test

--- portal/src/components/Input.jsx
+++ portal/src/components/Input.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
-    return <div data-test="component-input"></div>;
+    // do not destructure state if you want to mock it
+    const [currentGuess, setCurrentGuess] = React.useState('');
+
+    return (
+        <div data-test="component-input">
+            <form className="form-inline">
+                <input
+                    type="text"
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    placeholder="Enter guess"
+                    value={currentGuess}
+                    onChange={e => setCurrentGuess(e.target.value)}
+                />
+                <button
+                    type="button"
+                    data-test="guess-button"
+                    className="btn btn-primary mb-2">
+                    Guess
+                </button>
+            </form>
+        </div>
+    );
 };
 
 Input.propTypes = {
