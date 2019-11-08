5537be687154015bd324be1d4e3077b104350dd0
kboul <kos.voulgaridis@gmail.com>
2019-11-08 13:16:54 +0200

    Fetch secretWord on App using useReducer, getSecretWord action & write tests

--- portal/src/components/App.jsx
+++ portal/src/components/App.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import * as actions from '../actions/getSecretWord';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'setSecretWord':
+            return { ...state, secretWord: action.payload };
+        default:
+            throw new Error();
+    }
+}
+
+const initialState = { secretWord: null };
 
 const App = () => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    const setSecretWord = secretWord =>
+        dispatch({
+            type: 'setSecretWord',
+            payload: secretWord
+        });
+
+    React.useEffect(() => {
+        actions.getSecretWord(setSecretWord);
+    }, []);
+
+    console.log(state);
+
     return <div data-test="component-app">hello world</div>;
 };
 
--- portal/src/components/__tests__/App.test.js
+++ portal/src/components/__tests__/App.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../../../test/testUtils';
 import App from '../App';
+import * as actions from '../../actions/getSecretWord';
+
+const mockGetSecretWord = jest.fn();
 
 const setup = () => {
-    return shallow(<App />);
+    mockGetSecretWord.mockClear();
+    actions.getSecretWord = mockGetSecretWord;
+    // use mount because useEffect not called on shallow
+    return mount(<App />);
 };
 
 test('App renders without error', () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-app');
     expect(component).toHaveLength(1);
+});
+
+describe('getSecretWord calls', () => {
+    test('getSecretWord gets called on App mount', () => {
+        setup();
+
+        // check to see if secret word was updated
+        expect(mockGetSecretWord).toHaveBeenCalled();
+    });
 });
