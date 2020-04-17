c83927efb60f470df6d5eb84843db4d700573cc2
kboul <kos.voulgaridis@gmail.com>
2020-04-17 16:33:14 +0300

    Better style for Input

--- portal/src/components/Input.jsx
+++ portal/src/components/Input.jsx
@@ -6,10 +6,10 @@ import { toggleGiveUp } from '../store/actions';
 
 export class Input extends Component {
     state = {
-        currentGuess: ''
+        currentGuess: '',
     };
 
-    handleClick = e => {
+    handleClick = (e) => {
         const { currentGuess } = this.state;
         if (e && currentGuess.length > 0) {
             e.preventDefault();
@@ -24,36 +24,43 @@ export class Input extends Component {
         const { success, toggleGiveUp } = this.props;
         let contents;
         contents = !success ? (
-            <form className="form-inline">
-                <input
-                    type="text"
-                    data-test="input-box"
-                    className="form-control mb-2 mx-sm-3"
-                    placeholder="enter guess"
-                    value={currentGuess}
-                    onChange={e =>
-                        this.setState({ currentGuess: e.target.value })
-                    }
-                />
-                <button
-                    type="submit"
-                    data-test="submit-button"
-                    className="btn btn-primary mb-2"
-                    onClick={this.handleClick}>
-                    Guess
-                </button>
-                <button
-                    type="button"
-                    className="btn btn-danger mb-2 ml-2"
-                    data-test="give-up-button"
-                    onClick={() => toggleGiveUp()}>
-                    Give up
-                </button>
-            </form>
+            <div className="row align-items-end">
+                <div className="col-md-8">
+                    <input
+                        type="text"
+                        data-test="input-box"
+                        className="form-control"
+                        placeholder="enter guess"
+                        value={currentGuess}
+                        onChange={(e) =>
+                            this.setState({ currentGuess: e.target.value })
+                        }
+                    />
+                </div>
+
+                <div className="col-md-4 mt-2">
+                    <div className="btn-group">
+                        <button
+                            type="submit"
+                            data-test="submit-button"
+                            className="btn btn-primary"
+                            onClick={this.handleClick}>
+                            Guess
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-danger"
+                            data-test="give-up-button"
+                            onClick={() => toggleGiveUp()}>
+                            Give up
+                        </button>
+                    </div>
+                </div>
+            </div>
         ) : null;
 
         return (
-            <div className="col-md-6 offset-md-4" data-test="component-input">
+            <div className="col-md-6 offset-md-3" data-test="component-input">
                 {contents}
             </div>
         );
@@ -63,17 +70,14 @@ export class Input extends Component {
 Input.propTypes = {
     success: PropTypes.bool.isRequired,
     guessWord: PropTypes.func.isRequired,
-    toggleGiveUp: PropTypes.func.isRequired
+    toggleGiveUp: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({ success: state.success });
+const mapStateToProps = (state) => ({ success: state.success });
 
 const mapDispatchToProps = {
     guessWord,
-    toggleGiveUp
+    toggleGiveUp,
 };
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Input);
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
